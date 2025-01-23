5b13f4d32dd1e51ff0d4bcbcb01409654a48c9ca
Abner Daniel Fuentes Juárez <abnerfuentes05@gmail.com>
2025-01-23 14:09:15 -0600

    cambio en la pagina principal

--- views/pages/home.php
+++ views/pages/home.php
@@ -3,13 +3,13 @@
     <div id="header-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="w-100" src="images/otros/25.jpg" alt="Image">
+                <img class="w-100" src="images/carousel/1.JPG" alt="Image">
                 <div class="carousel-caption">
                     <div class="container">
                         <div class="row justify-content-start">
                             <div class="col-lg-7 text-start">
                                 <h1 class="display-1 text-white animate__animated animate__slideInRight mb-3">
-                                    Industria Militar</h1>
+                                    CONAMAR</h1>
                                 <!-- <p class="fs-4 mb-5 animated slideInRight">Respetamos y protegemos la dignidad humana.</p> -->
                             </div>
                         </div>
@@ -17,13 +17,13 @@
                 </div>
             </div>
             <div class="carousel-item">
-                <img class="w-100" src="images/otros/20.jpg" alt="Image">
+                <img class="w-100" src="images/carousel/1.JPG" alt="Image">
                 <div class="carousel-caption">
                     <div class="container">
                         <div class="row justify-content-end">
                             <div class="col-lg-7 text-end">
                                 <h1 class="display-1 text-white animate__animated animate__slideInLeft mb-3">
-                                    Industria Militar</h1>
+                                    CONAMAR</h1>
                                 <!-- <p class="fs-4 mb-5 animate__animated animate__slideInLeft">Respetamos y protegemos la dignidad humana.</p> -->
                             </div>
                         </div>
@@ -31,13 +31,13 @@
                 </div>
             </div>
             <div class="carousel-item">
-                <img class="w-100" src="images/otros/12.jpg" alt="Image">
+                <img class="w-100" src="images/carousel/1.JPG" alt="Image">
                 <div class="carousel-caption">
                     <div class="container">
                         <div class="row justify-content-end">
                             <div class="col-lg-7 text-end">
                                 <h1 class="display-1 text-white animate__animated animate__slideInRight mb-3">
-                                    Industria Militar</h1>
+                                    CONAMAR</h1>
                                 <!-- <p class="fs-4 mb-5 animate__animated animate__slideInLeft">Respetamos y protegemos la dignidad humana.</p> -->
                             </div>
                         </div>
@@ -45,13 +45,13 @@
                 </div>
             </div>
             <div class="carousel-item">
-                <img class="w-100" src="images/otros/06.jpg" alt="Image">
+                <img class="w-100" src="images/carousel/1.JPG" alt="Image">
                 <div class="carousel-caption">
                     <div class="container">
                         <div class="row justify-content-end">
                             <div class="col-lg-7 text-end">
                                 <h1 class="display-1 text-white animate__animated animate__slideInLeft mb-3">
-                                    Industria Militar</h1>
+                                    CONAMAR</h1>
                                 <!-- <p class="fs-4 mb-5 animate__animated animate__slideInLeft">Respetamos y protegemos la dignidad humana.</p> -->
                             </div>
                         </div>
@@ -59,13 +59,13 @@
                 </div>
             </div>
             <div class="carousel-item">
-                <img class="w-100" src="images/otros/10.jpg" alt="Image">
+                <img class="w-100" src="images/carousel/1.JPG" alt="Image">
                 <div class="carousel-caption">
                     <div class="container">
                         <div class="row justify-content-end">
                             <div class="col-lg-7 text-end">
                                 <h1 class="display-1 text-white animate__animated animate__slideInRight mb-3">
-                                    Industria Militar</h1>
+                                    CONAMAR</h1>
                                 <!-- <p class="fs-4 mb-5 animate__animated animate__slideInLeft">Respetamos y protegemos la dignidad humana.</p> -->
                             </div>
                         </div>
@@ -88,24 +88,21 @@
 <div class="container-fluid container-team py-5">
     <div class="container pb-5">
         <div class="row g-5 align-items-center mb-5">
-            <div class="col-md-6 wow fadeIn" data-wow-delay="0.3s">
-                <img class="img-fluid w-100" src="images/director.png" alt="">
+            <div class="col-md-6 wow animate__animated animate__fadeIn" data-wow-delay="0.3s">
+                <img class="img-fluid w-100" src="images/otros/viceministro_marina.png" alt="">
             </div>
             <div class="col-md-6 wow animate__animated animate__fadeIn" data-wow-delay="0.5s">
-                <h1 class="display-6 mb-3">Coronel de Sanidad Militar DEM Karen Silvana Pérez Meléndez</h1>
-                <p class="mb-1 text-muted">Jefa de la Industria Militar</p>
-                <p class="mb-2 text-muted">Ministerio de la Defensa Nacional</p>
+                <h1 class="display-6 mb-3">Vicealmirante Edy Santiago Chinchilla Archila</h1>
+                <p class="mb-1 text-muted">Viceministro de Marina del MDN</p>
+                <p class="mb-2 text-muted">Presidente y Coordinador de la CONAMAR</p>
                 <h2 class="mb-3">Bienvenidos</h2>
-                <p class="mb-4 fs-4">Me complace dirigirme a todos ustedes como la primera mujer nombrada en el puesto
-                    de Jefe de la Industria Militar del Ejército de Guatemala. Es un honor y un privilegio asumir este
-                    cargo de liderazgo y trabajar junto a cada uno de ustedes en el impulso y fortalecimiento de nuestra
-                    valiosa Industria Militar, agradeciendo primordialmente a Dios y al Mando del Ejército de Guatemala
-                    por haber depositado la confianza en mí en tan prestigioso cargo.</p>
-                <p class="mb-4 fs-4">Me comprometo a promover una cultura de rendición de cuentas en la que todos los
-                    miembros del equipo de trabajo sean responsables y estén comprometidos en todas las áreas de nuestra
-                    organización. Además, velaré por la uniformidad de todo el personal militar. Reconozco la
-                    importancia de contar con un equipo cohesionado y disciplinado, donde todos compartamos los mismos
-                    valores y objetivos.</p>
+                <p class="mb-4 fs-4">¡Bienvenidos a la página web de la Comisión Nacional de Administración Marítima de
+                    Guatemala! Nos complace ofrecerles un espacio donde podrán conocer más sobre nuestra misión, visión
+                    y el trabajo que realizamos para coordinar y fortalecer la gestión de los recursos marítimos de
+                    nuestro país. A través de la cooperación interinstitucional y la implementación de políticas y
+                    estrategias eficaces, buscamos garantizar el desarrollo sostenible, la seguridad y la protección de
+                    nuestros espacios marítimos. Les invitamos a explorar nuestra plataforma y a informarse
+                    sobre las iniciativas</p>
                 <div class="d-flex">
                     <a class="btn btn-lg-square btn-primary me-2" href=""><i class="fab fa-facebook-f"></i></a>
                     <a class="btn btn-lg-square btn-primary me-2" href=""><i class="fab fa-twitter"></i></a>
@@ -117,60 +114,76 @@
     </div>
 </div>
 
-<!-- About Start -->
-<div class="container-fluid py-5">
-    <div class="container">
-        <div class="row g-5">
-            <div class="col-lg-6 wow animate__animated animate__fadeIn" data-wow-delay="0.1s">
-                <div class="row g-0">
-                    <div class="col-6">
-                        <img class="img-fluid" src="./images/about1.jpg">
+<div class="container-fluid container-service py-5">
+    <h1 class="display-5 text-center">Instituciones que integran la CONAMAR</h1>
+    <div class="container-fluid py-5">
+        <div class="row justify-content-around g-4 mb-3">
+            <div class="col-lg-3 col-md-6 wow animate__animated animate__fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item d-flex flex-column justify-content-between">
+                    <div class="icon-box-primary mb-4 h-100 d-flex flex-column justify-content-center">
+                        <img src="./images/logos/MINDEF.png" class="w-100" alt="">
                     </div>
-                    <div class="col-6">
-                        <img class="img-fluid" src="./images/about2.jpg">
-                    </div>
-                    <div class="col-6">
-                        <img class="img-fluid" src="./images/about3.jpg">
-                    </div>
-                    <div class="col-6">
-                        <div
-                            class="bg-primary w-100 h-100 mt-n5 ms-n5 d-flex flex-column align-items-center justify-content-center">
-                            <div class="icon-box-light">
-                                <i class="bi bi-award text-dark"></i>
-                            </div>
-                            <h1 class="display-1 text-white mb-0" data-toggle="counter-up">25</h1>
-                            <small class="fs-5 text-white">Años de experiencia</small>
-                        </div>
-                    </div>
+                    <h5 class="mb-3">Ministerio de la Defensa Nacional</h4>
                 </div>
             </div>
-            <div class="col-lg-6 wow animate__animated animate__fadeIn" data-wow-delay="0.5s">
-                <h1 class="display-6 mb-4">Industria Militar</h1>
-                <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tellus augue, iaculis
-                    id elit eget, ultrices pulvinar tortor. Quisque vel lorem porttitor, malesuada arcu quis, fringilla
-                    risus. Pellentesque eu consequat augue.</p>
-                <div class="row g-4 g-sm-5 justify-content-center">
-                    <div class="col-sm-6">
-                        <div class="about-fact btn-square flex-column rounded-circle bg-primary ms-sm-auto">
-                            <p class="text-white mb-0">Premios ganados</p>
-                            <h1 class="text-white mb-0" data-toggle="counter-up">9999</h1>
-                        </div>
+            <div class="col-lg-3 col-md-6 wow animate__animated animate__fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item d-flex flex-column justify-content-between">
+                    <div class="icon-box-primary mb-4 h-100 d-flex flex-column justify-content-center">
+                        <img src="./images/logos/MAGA.png" class="w-100" alt="">
                     </div>
-                    <div class="col-sm-6 text-start">
-                        <div class="about-fact btn-square flex-column rounded-circle bg-secondary me-sm-auto">
-                            <p class="text-white mb-0">Uniformes costurados</p>
-                            <h1 class="text-white mb-0" data-toggle="counter-up">9999</h1>
-                        </div>
+                    <h5 class="mb-3">Ministerio de Agricultura Ganadería y Alimentación</h4>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 wow animate__animated animate__fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item d-flex flex-column justify-content-between">
+                    <div class="icon-box-primary mb-4 h-100 d-flex flex-column justify-content-center">
+                        <img src="./images/logos/MARN.png" class="w-100" alt="">
                     </div>
-                    <div class="col-sm-6">
-                        <div class="about-fact mt-n130 btn-square flex-column rounded-circle bg-dark mx-sm-auto">
-                            <p class="text-white mb-0">Clientes satisfechos</p>
-                            <h1 class="text-white mb-0" data-toggle="counter-up">9999</h1>
-                        </div>
+                    <h5 class="mb-3">Ministerio de Ambiente y Recursos Naturales</h4>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 wow animate__animated animate__fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item d-flex flex-column justify-content-between">
+                    <div class="icon-box-primary mb-4 h-100 flex-1 d-flex flex-column justify-content-center">
+                        <img src="./images/logos/CIV.png" class="w-100" alt="">
                     </div>
+                    <h5 class="mb-3">Ministerio de Comunicaciones Infraestructura y Vivienda</h4>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-around g-4 mb-3">
+            <div class="col-lg-3 col-md-6 wow animate__animated animate__fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item d-flex flex-column justify-content-between">
+                    <div class="icon-box-primary mb-4 h-100 d-flex flex-column justify-content-center">
+                        <img src="./images/logos/MINDEF.png" class="w-100" alt="">
+                    </div>
+                    <h5 class="mb-3">Ministerio de Economía</h4>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 wow animate__animated animate__fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item d-flex flex-column justify-content-between">
+                    <div class="icon-box-primary mb-4 h-100 d-flex flex-column justify-content-center">
+                        <img src="./images/logos/MEM.png" class="w-100" alt="">
+                    </div>
+                    <h5 class="mb-3">Ministerio de Energía y Minas</h4>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 wow animate__animated animate__fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item d-flex flex-column justify-content-between">
+                    <div class="icon-box-primary mb-4 h-100 d-flex flex-column justify-content-center">
+                        <img src="./images/logos/MINEX.png" class="w-100" alt="">
+                    </div>
+                    <h5 class="mb-3">Ministerio de Relaciones Exteriores</h4>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 wow animate__animated animate__fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item d-flex flex-column justify-content-between">
+                    <div class="icon-box-primary mb-4 h-100 flex-1 d-flex flex-column justify-content-center">
+                        <img src="./images/logos/CONAP.png" class="w-100" alt="">
+                    </div>
+                    <h5 class="mb-3">Consejo Nacional de Areas Protegidas</h4>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<!-- About End -->
