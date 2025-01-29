25242bf9d54d7551153131072459fa89687c65ff
Abner Daniel Fuentes Juárez <abnerfuentes05@gmail.com>
2025-01-29 09:40:22 -0600

    CORRECCION EN LA PRIMERA IMAGEN DEL CAROUSEL

--- views/pages/home.php
+++ views/pages/home.php
@@ -3,7 +3,7 @@
     <div id="header-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="w-100" src="./images/carousel/1.jpg" alt="Image">
+                <img class="w-100" src="./images/carousel/1.JPG" alt="Image">
                 <div class="carousel-caption">
                     <div class="container">
                         <div class="row justify-content-start">
